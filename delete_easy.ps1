9c2a56a6fb42845b6c02cc63298fd8690876a307
walvok <49453576+walvok@users.noreply.github.com>
2021-01-02 23:16:07 +0100

    Update delete_easy.ps1

--- delete_easy.ps1
+++ delete_easy.ps1
@@ -1,6 +1,6 @@
 DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:PACKAGENAME
-#Odinstaluj OneDrive:
+#uninstall OneDrive:
 ps onedrive | Stop-Process -Force
 start-process "$env:windir\SysWOW64\OneDriveSetup.exe" "/uninstall"
 
